67554d4f750126c120663cb1ec9891d18868ca3a
Ugur Guliyev <ugurguliyev@Ugurs-MacBook-Air.local>
2020-08-05 03:48:55 +0400

    new look

--- js/main.js
+++ js/main.js
@@ -1,77 +1,82 @@
-$(".container").hide();
+$(".body-div").hide();
 
 $(document).ready(() => {
-    
-    let mainUrl = "https://www.omdbapi.com/?apikey=647b7461";
+  let mainUrl = "https://www.omdbapi.com/?apikey=647b7461";
 
-    // Elements
+  // Elements
 
-    let $srchBtn = $(".btn-search");
-    let $titleOfMovie = $("#titleOfMovie");
+  let $srchBtn = $(".btn-search");
+  let $titleOfMovie = $("#titleOfMovie");
 
-    // Function for getting API
+  // Function for getting API
 
-    $titleOfMovie.keydown(function(event) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
+  $titleOfMovie.keydown(function (event) {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+    }
+  });
+
+  $srchBtn.on("click", () => {
+    function getMovieByTitle(url, title) {
+      $(".poster-img").empty();
+      $("#imdbRating").empty();
+      $("#metascoreRating").empty();
+      $(".info").empty();
+
+      let resultUrl = `${url}&t=${title}`;
+      fetch(resultUrl)
+        .then((response) => response.json())
+        .then((data) => DataOfMovie(data));
+    }
+
+    getMovieByTitle(mainUrl, $titleOfMovie.val());
+
+    function DataOfMovie(data) {
+      for (let value in data) {
+        if (value === "Title") {
+          $(".title-of-movie").append(`${data["Title"]}`);
+        } else if (value === "Genre") {
+          $(".genre-of-movie").append(data["Genre"]);
+        } else if (value === "imdbRating") {
+          $(".rating-of-movie").append(
+            `${data["imdbRating"]} | ${data["Runtime"]}`
+          );
+          $("#imdbRating").append(data["imdbRating"]);
+        } else if (value === "Metascore") {
+          $("#metascoreRating").append(data["Metascore"]);
+        } else if (value === "Plot") {
+          $(".plot-of-movie").append(data["Plot"]);
+        } else if (value === "Released") {
+          $("#release-date").append(`Release date : ${data["Released"]}`);
+        } else if (value === "Director") {
+          $("#director").append(`Director : ${data["Director"]}`);
+        } else if (value === "BoxOffice") {
+          $("#box-office").append(`Box office : ${data["BoxOffice"]}`);
+        } else if (value === "Rated") {
+          $("#rated").append(`Rated : ${data["Rated"]}`);
+        } else if (value === "Actors") {
+          $("#cast").append(`Cast : ${data["Actors"]}`);
+        } else if (value === "Poster") {
+          let str = data["Poster"];
+          let n = str.search("300");
+          str = setCharAt(str, n, "10");
+          console.log(str);
+
+          console.log(n);
+
+          function setCharAt(str, index, chr) {
+            if (index > str.length - 1) return str;
+            return str.substr(0, index) + chr + str.substr(index + 1);
+          }
+
+          $(".poster-img").append(`<img src="${str}">`);
         }
-    });
+        value = [];
+      }
+    }
 
-    // EventListener
+    $(".body-div").show();
 
-    $srchBtn.on('click', () => {
-
-
-        function getMovieByTitle(url, title){
-            $(".poster-img").empty();
-            $("#imdbRating").empty();
-            $("#metascoreRating").empty();
-            $(".info").empty();
-
-            let resultUrl = `${url}&t=${title}`;
-            fetch(resultUrl).then(response => response.json()).then(data => DataOfMovie(data));
-        }
-
-        getMovieByTitle(mainUrl, $titleOfMovie.val());
-
-
-        function DataOfMovie(data){
-            for(let value in data){
-                if (value === "Title"){
-                    $(".title-of-movie").append(data["Title"]);
-                }else if(value === "Genre"){
-                    $(".genre-of-movie").append(data["Genre"]);
-                }else if (value === "imdbRating"){
-                    $(".rating-of-movie").append(`${data["imdbRating"]} | ${data["Runtime"]}`);
-                    $("#imdbRating").append(data["imdbRating"]);
-                }else if (value === "Metascore"){
-                    $("#metascoreRating").append(data["Metascore"]);
-                }
-                else if (value === "Plot"){
-                    $(".plot-of-movie").append(data["Plot"]);
-                }else if (value === "Released"){
-                    $("#release-date").append(`Release date : ${data["Released"]}`);
-                }else if(value === "Director"){
-                    $("#director").append(`Director : ${data["Director"]}`);
-                }else if(value === "BoxOffice" ){
-                    $("#box-office").append(`Box office : ${data["BoxOffice"]}`);
-                }else if (value === "Rated"){
-                    $("#rated").append(`Rated : ${data["Rated"]}`);
-                }else if (value === "Actors"){
-                    $("#cast").append(`Cast : ${data["Actors"]}`);
-                }else if(value === "Poster"){
-                    $(".poster-img").append(`<img src="${data["Poster"]}">`);
-                }
-                value = [];
-            }
-        }
-
-        
-
-        $(".container").show();
-
-        $titleOfMovie.val("");
-    });
-
+    $titleOfMovie.val("");
+  });
 });
-
